f1cbd69a0a8c42fbe8b97b2f01b891f8e5f28837
Laurent VERDOÏA <verdoialaurent@gmail.com>
2025-03-07 08:38:55 +0100

    Refactor healthcheck command
    - Correctly invoke internal/app/cmd/healthcheck.Run from cmd.healthCheckCmd
    - Setup User-Agent to demo-backend-go/<revision>

--- cmd/healthcheck.go
+++ cmd/healthcheck.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-	"net/http"
-	"os"
+	"go.lvjp.me/demo-backend-go/internal/app/cmd/healthcheck"
 
 	"github.com/spf13/cobra"
 )
@@ -12,19 +10,7 @@ var healthCheckCmd = &cobra.Command{
 	Use:   "healthcheck",
 	Short: "Check the health of the server",
 	Run: func(*cobra.Command, []string) {
-		resp, err := http.Get("http://localhost:8080/api/v0/misc/version")
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		defer resp.Body.Close()
-
-		fmt.Println(resp.Status)
-
-		if resp.StatusCode != http.StatusOK {
-			os.Exit(1)
-		}
+		healthcheck.Run()
 	},
 }
 
--- internal/app/cmd/healthcheck/main.go
+++ internal/app/cmd/healthcheck/main.go
@@ -4,24 +4,35 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"go.lvjp.me/demo-backend-go/pkg/buildinfo"
 )
 
-func Run() error {
-	endpoint := "http://:8080"
-	resp, err := http.Get(endpoint)
+func Run() {
+	endpoint := "http://localhost:8080/api/v0/misc/version"
+
+	req, err := http.NewRequest(http.MethodHead, endpoint, nil)
+	if err != nil {
+		fmt.Println("Cannot forge the healthcheck request:", err.Error())
+		os.Exit(1)
+	}
+
+	req.Header.Set("User-Agent", "demo-backend-go/"+buildinfo.Get().Revision)
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	defer resp.Body.Close()
+	// HEAD request does not have a body
+	resp.Body.Close()
 
 	fmt.Println("Endpoint:", endpoint)
 	fmt.Println("Status:", resp.Status)
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		fmt.Println("unexpected status code:", resp.StatusCode)
+		os.Exit(1)
 	}
-
-	return nil
 }
